cd37a9924e5380536a16eb0e4a32f570775048f1
Adedimeji Ojerinde <ojerindeadedimeji@gmail.com>
2025-01-16 17:23:00 -0800

    .

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -7,6 +7,6 @@ class Book:
         self.author = author
         self.publication_year = publication_year
 
-    title =  CharField(max_length=200)
-    author = CharField(max_length=100)
-    publication_year = IntegerField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
